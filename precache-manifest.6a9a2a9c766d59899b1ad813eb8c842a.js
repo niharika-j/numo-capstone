05c3347ebd86d69d5c873179d68d71822a6ac2bb
Niharika Jayanthi <niharika.jayanthi06@gmail.com>
2020-05-04 21:54:53 -0400

    Updates

---
+++ precache-manifest.6a9a2a9c766d59899b1ad813eb8c842a.js
@@ -0,0 +1,302 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30970f2ea018c500167c9ad6da5fae1",
+    "url": "/numo-capstone/index.html"
+  },
+  {
+    "revision": "456845839fac6579e4a3",
+    "url": "/numo-capstone/static/css/2.5280a38c.chunk.css"
+  },
+  {
+    "revision": "7a25360f8d1e0da9f350",
+    "url": "/numo-capstone/static/css/main.d1d8b7a3.chunk.css"
+  },
+  {
+    "revision": "456845839fac6579e4a3",
+    "url": "/numo-capstone/static/js/2.a573de50.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/numo-capstone/static/js/2.a573de50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a25360f8d1e0da9f350",
+    "url": "/numo-capstone/static/js/main.67abfefe.chunk.js"
+  },
+  {
+    "revision": "af33f4f55e1b35bd7c2b",
+    "url": "/numo-capstone/static/js/runtime-main.339215f8.js"
+  },
+  {
+    "revision": "80d3ff291782007ed26a5901bd67b0a1",
+    "url": "/numo-capstone/static/media/about-us-bg.80d3ff29.png"
+  },
+  {
+    "revision": "9052285e1eb5c863017b73607812094a",
+    "url": "/numo-capstone/static/media/about-us-landing-bg.9052285e.png"
+  },
+  {
+    "revision": "b8e418458b3ae69148938523c46568a8",
+    "url": "/numo-capstone/static/media/background-research-header-bg.b8e41845.png"
+  },
+  {
+    "revision": "295aff738f64789d2ea8ada7a14c8f79",
+    "url": "/numo-capstone/static/media/bg-approach-1.295aff73.png"
+  },
+  {
+    "revision": "a734f68572a099c22eac0bfb057eea00",
+    "url": "/numo-capstone/static/media/bin-grey.a734f685.png"
+  },
+  {
+    "revision": "ae113c0237c9fd35bffd53f82c9bc3ef",
+    "url": "/numo-capstone/static/media/bin.ae113c02.png"
+  },
+  {
+    "revision": "3d1523115ef5b177ccf5a68535f2a827",
+    "url": "/numo-capstone/static/media/booth-interview.3d152311.png"
+  },
+  {
+    "revision": "5d85e3350893948563f20e50a9b31706",
+    "url": "/numo-capstone/static/media/conceptual-prototype-img.5d85e335.png"
+  },
+  {
+    "revision": "ab4d814facb8f817cee2e74d52060429",
+    "url": "/numo-capstone/static/media/consumer-landing-bg.ab4d814f.png"
+  },
+  {
+    "revision": "a28537eb06aba1fe5756e0c60b270136",
+    "url": "/numo-capstone/static/media/consumer-persona-1.a28537eb.jpg"
+  },
+  {
+    "revision": "d9beebe568e6bd7213264b2bbc67b1b8",
+    "url": "/numo-capstone/static/media/consumer-persona-2.d9beebe5.jpg"
+  },
+  {
+    "revision": "2cac49b52f94fbfea8d0bfd7bd1da5ab",
+    "url": "/numo-capstone/static/media/consumer-persona-3.2cac49b5.jpg"
+  },
+  {
+    "revision": "8c7efeaceb8434e9b7e82444c8dba773",
+    "url": "/numo-capstone/static/media/consumer-persona-4.8c7efeac.jpg"
+  },
+  {
+    "revision": "ae3b4e9ace92cadb00b10df9d20be54a",
+    "url": "/numo-capstone/static/media/consumer-research.ae3b4e9a.png"
+  },
+  {
+    "revision": "a9d15e94c465f96e69210a40cc91c31e",
+    "url": "/numo-capstone/static/media/consumer-speed-dating-bg.a9d15e94.png"
+  },
+  {
+    "revision": "5b0e355e3e83bd6401e9bc2fa61aa8e9",
+    "url": "/numo-capstone/static/media/consumer-superuser-bg.5b0e355e.png"
+  },
+  {
+    "revision": "0946cf4175d8110658f0780ec768295c",
+    "url": "/numo-capstone/static/media/guerilla-research.0946cf41.png"
+  },
+  {
+    "revision": "e852712575dc1af4001e3b7372baf8e9",
+    "url": "/numo-capstone/static/media/ideating-bg.e8527125.png"
+  },
+  {
+    "revision": "bc2b9661520a3e3ea6c3738af1aa04d3",
+    "url": "/numo-capstone/static/media/interpretation-session-bg.bc2b9661.png"
+  },
+  {
+    "revision": "c2b58d15b68b6c0f12e2890284b18682",
+    "url": "/numo-capstone/static/media/investment-consumer-1.c2b58d15.png"
+  },
+  {
+    "revision": "172d4bcae1e7464b01644ced24ab3ee7",
+    "url": "/numo-capstone/static/media/investment-consumer-2.172d4bca.png"
+  },
+  {
+    "revision": "ec2fd0848a9437f7caecb4e66a75cfe2",
+    "url": "/numo-capstone/static/media/investment-consumer-3.ec2fd084.png"
+  },
+  {
+    "revision": "8ce5028731a7507842363c2b96dc970d",
+    "url": "/numo-capstone/static/media/investment-merchant-1.8ce50287.png"
+  },
+  {
+    "revision": "028792db37878a0f448509b0cc62341c",
+    "url": "/numo-capstone/static/media/investment-merchant-2.028792db.png"
+  },
+  {
+    "revision": "485f67ed7bde5b711cdf03c13d1e5d74",
+    "url": "/numo-capstone/static/media/kyle-grey.485f67ed.png"
+  },
+  {
+    "revision": "84fc526ff74aa8df5b4369ddc7675747",
+    "url": "/numo-capstone/static/media/kyle.84fc526f.png"
+  },
+  {
+    "revision": "bd6515a7855df30bc72e771b590d15f7",
+    "url": "/numo-capstone/static/media/landing-bg.bd6515a7.png"
+  },
+  {
+    "revision": "263f7e197b579f08c76f8b1c62651e22",
+    "url": "/numo-capstone/static/media/lauren-grey.263f7e19.png"
+  },
+  {
+    "revision": "b285ed4d8b637e17b692cbc4b3611e4c",
+    "url": "/numo-capstone/static/media/lauren.b285ed4d.png"
+  },
+  {
+    "revision": "c596cd85321a83030527f211a79b3c6a",
+    "url": "/numo-capstone/static/media/medium-bg.c596cd85.png"
+  },
+  {
+    "revision": "6a60ee6794f4a9edfb6603b29ce9a6a2",
+    "url": "/numo-capstone/static/media/merchant-interpretation-session.6a60ee67.png"
+  },
+  {
+    "revision": "09eef79e073bca86c04ef5689af159a6",
+    "url": "/numo-capstone/static/media/merchant-persona-1.09eef79e.jpg"
+  },
+  {
+    "revision": "224c42d2bbe9b05181a908e8f73f1ee2",
+    "url": "/numo-capstone/static/media/merchant-persona-2.224c42d2.jpg"
+  },
+  {
+    "revision": "217ad6f4edeb1ad7c21040e9603bd1fa",
+    "url": "/numo-capstone/static/media/merchant-research.217ad6f4.png"
+  },
+  {
+    "revision": "8ddbde28bd57a56dc2015c39b2230ffe",
+    "url": "/numo-capstone/static/media/merchant-title-bg.8ddbde28.png"
+  },
+  {
+    "revision": "7e7fca9f687178f72d57fd54de48cd56",
+    "url": "/numo-capstone/static/media/merchant2.7e7fca9f.png"
+  },
+  {
+    "revision": "bf61c8b39d91de7fe6a7f3eb385f2196",
+    "url": "/numo-capstone/static/media/merchant3.bf61c8b3.png"
+  },
+  {
+    "revision": "20d48299eaae9bc4a7135340415b40cb",
+    "url": "/numo-capstone/static/media/merchant5.20d48299.png"
+  },
+  {
+    "revision": "bbd4920be2dbfbdeede2edcfcbee0bc7",
+    "url": "/numo-capstone/static/media/merchant6.bbd4920b.png"
+  },
+  {
+    "revision": "9768e1c1b6eb0f442399c6280c935390",
+    "url": "/numo-capstone/static/media/mia-grey.9768e1c1.png"
+  },
+  {
+    "revision": "ee3cf0fca544494d084a0084181857ac",
+    "url": "/numo-capstone/static/media/mia.ee3cf0fc.png"
+  },
+  {
+    "revision": "655b9c2093b660e42e9bcc16499db168",
+    "url": "/numo-capstone/static/media/niharika-grey.655b9c20.png"
+  },
+  {
+    "revision": "34c0edb6fbe0e7bee185df72af6baa18",
+    "url": "/numo-capstone/static/media/niharika.34c0edb6.png"
+  },
+  {
+    "revision": "068709d28e40d1723b16f5c4eed1f9fc",
+    "url": "/numo-capstone/static/media/primary-research-bg.068709d2.png"
+  },
+  {
+    "revision": "748650b13f82178a4ac1fbe8faadd259",
+    "url": "/numo-capstone/static/media/prototypes-bg.748650b1.png"
+  },
+  {
+    "revision": "367326325fc96c11b88b3c12408931a9",
+    "url": "/numo-capstone/static/media/prototypes-landing-bg.36732632.png"
+  },
+  {
+    "revision": "d0de3d18544c8689d276ab5fb75fafc6",
+    "url": "/numo-capstone/static/media/prototyping-bg.d0de3d18.png"
+  },
+  {
+    "revision": "8b75a3c44a3aa4626c54135808b6690c",
+    "url": "/numo-capstone/static/media/refined-prototype-1.8b75a3c4.png"
+  },
+  {
+    "revision": "e7ffec0f41a36b2aecd6525acbe6d703",
+    "url": "/numo-capstone/static/media/refined-prototype-2.e7ffec0f.png"
+  },
+  {
+    "revision": "24fa37bac5d0f2b45b58344b2b138028",
+    "url": "/numo-capstone/static/media/restaurant-consumer-1.24fa37ba.png"
+  },
+  {
+    "revision": "ff142ad68dd605339c8eb6986efc5286",
+    "url": "/numo-capstone/static/media/restaurant-consumer-2.ff142ad6.png"
+  },
+  {
+    "revision": "9cbcbd956a80b51d341c9e4872edf0b6",
+    "url": "/numo-capstone/static/media/restaurant-consumer-3.9cbcbd95.png"
+  },
+  {
+    "revision": "d28b437c671d52875107137bd999dbe4",
+    "url": "/numo-capstone/static/media/restaurant-merchant-1.d28b437c.png"
+  },
+  {
+    "revision": "1330c067641be9d2542c8f212471ceaa",
+    "url": "/numo-capstone/static/media/restaurant-merchant-2.1330c067.png"
+  },
+  {
+    "revision": "16181ca2405d00fe14afe8fbb4787c29",
+    "url": "/numo-capstone/static/media/semi-structured-interviews-bg.16181ca2.png"
+  },
+  {
+    "revision": "fad4830de2abe871c83ee2922f589d59",
+    "url": "/numo-capstone/static/media/semi-structured-merchant-interview.fad4830d.png"
+  },
+  {
+    "revision": "f38017e577314728f8fc7f1c4b032663",
+    "url": "/numo-capstone/static/media/speed-dating-2.f38017e5.png"
+  },
+  {
+    "revision": "900adbc2071325fd551e9169762e2e5f",
+    "url": "/numo-capstone/static/media/speed-dating-3.900adbc2.png"
+  },
+  {
+    "revision": "696929d8d99f230f9495a767e6db763b",
+    "url": "/numo-capstone/static/media/speed-dating.696929d8.png"
+  },
+  {
+    "revision": "59c0265db7c58b36e71170ec4016edf8",
+    "url": "/numo-capstone/static/media/stakeholder-map.59c0265d.png"
+  },
+  {
+    "revision": "136abc69a6d6aee40916943e98679e5e",
+    "url": "/numo-capstone/static/media/storyboards-2.136abc69.png"
+  },
+  {
+    "revision": "5621717a1b6f04aaa6be567f254c60e9",
+    "url": "/numo-capstone/static/media/storyboards-3.5621717a.png"
+  },
+  {
+    "revision": "bc091532f3bc7f31c0e443b269d4fd6d",
+    "url": "/numo-capstone/static/media/storyboards.bc091532.png"
+  },
+  {
+    "revision": "6884afdbcfc216c3594dfc1c870fa3f2",
+    "url": "/numo-capstone/static/media/takeaways-clo.6884afdb.png"
+  },
+  {
+    "revision": "cd89af678038549843c630af6be0097a",
+    "url": "/numo-capstone/static/media/takeaways-publisher.cd89af67.png"
+  },
+  {
+    "revision": "f3ee4d521947db5526c4ff303728daf4",
+    "url": "/numo-capstone/static/media/talk-to-consumers-bg.f3ee4d52.png"
+  },
+  {
+    "revision": "69f89742d86d2bc2f2921e66725ea9ad",
+    "url": "/numo-capstone/static/media/validate-assumptions-bg.69f89742.png"
+  },
+  {
+    "revision": "6f68b4a6185e0db7ba0f5e8aad97baa8",
+    "url": "/numo-capstone/static/media/what-is-clo.6f68b4a6.png"
+  }
+]);
